6a7d1b068481ae7810c621b362e5d862921e3fdb
psajaev1 <psajaev@gmail.com>
2022-07-19 17:52:14 -0500

    Update solidity versions and add Pair.sol

---
+++ src/Pair.sol
@@ -0,0 +1,10 @@
+pragma solidity ^0.8.9;
+
+import "solmate/tokens/ERC20.sol";
+import "./libraries/Math.sol";
+
+interface IERC20 {
+    function balanceOf(address) external returns (uint256);
+
+    function transfer(address to, uint256 amount) external;
+}
