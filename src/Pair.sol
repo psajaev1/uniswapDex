1e1d9aa0d4f7297c2e2d150e88d461cdff3e4e9b
psajaev1 <psajaev@gmail.com>
2022-07-25 15:42:39 -0500

    Add token swapping

--- src/Pair.sol
+++ src/Pair.sol
@@ -3,10 +3,16 @@ pragma solidity ^0.8.9;
 
 import "solmate/tokens/ERC20.sol";
 import "./libraries/Math.sol";
+import "./libraries/UQ112x112.sol";
+
 
 error InsufficientLiquidityMinted();
 error InsufficientLiquidityBurned();
 error TransferFailed();
+error InsufficientOutputAmount();
+error InsufficientLiquidity();
+error InvalidProduct();
+error BalanceOverflow();
 
 interface IERC20 {
     function balanceOf(address) external returns (uint256);
@@ -23,6 +29,11 @@ contract Pair is ERC20, Math {
 
     uint112 private reserve0;
     uint112 private reserve1;
+
+    uint32 private blockTimestampLast;
+
+    uint256 public price0CumulativeLast;
+    uint256 public price1CumulativeLast;
 
     event Burn(address indexed sender, uint256 amount0, uint256 amount1);
     event Mint(address indexed sender, uint256 amount0, uint256 amount1);
@@ -74,10 +85,68 @@ contract Pair is ERC20, Math {
 
     }
 
-    function _update(uint256 balance0, uint256 balance1) private {
+
+    function swap(uint256 amountOut0, uint256 amountOut1, address to) public {
+        if (amountOut0 == 0 && amountOut1 == 0){
+            revert InsufficientOutputAmount();
+        }
+
+        (uint112 _reserve0, uint112 _reserve1, ) = getReserves();
+
+        if (amountOut0 > _reserve0 || amountOut1 > _reserve1){
+            revert InsufficientLiquidity();
+        }
+
+        uint256 balance0 = IERC20(token0).balanceOf(address(this)) - amountOut0;
+        uint256 balance1 = IERC20(token1).balanceOf(address(this)) - amountOut1;
+
+        if (balance0 * balance1 < uint256(_reserve0) * uint256(_reserve1)){
+            revert InvalidProduct();
+        }
+
+        _update(balance0, balance1, _reserve0, _reserve1);
+
+        if (amountOut0 > 0) {
+            _safeTransfer(token0, to, amountOut0);
+        }
+
+        if (amountOut1 > 0){
+            _safeTransfer(token1, to, amountOut1);
+        }
+
+        emit Swap(msg.sender, amountOut0, amountOut1, to);
+
+    }
+
+    function getReserves() public view returns (
+        uint112,
+        uint112,
+        uint32
+    )
+    {
+        return (reserve0, reserve1, 0);
+    }
+
+    function _update(uint256 balance0, uint256 balance1, uint112 _reserve0, uint112 _reserve1) private {
+        
+        if (balance0 > type(uint112).max || balance1 > type(uint112).max){
+            revert BalanceOverflow();
+        }
+
+        unchecked {
+            uint32 timeElapsed = uint32(block.timestamp) - blockTimestampLast;
+
+            if (timeElapsed > 0 && _reserve0 > 0 && _reserve1 > 0){
+                price0CumulativeLast += uint256(UQ112x112.encode(_reserve1).uqdiv(_reserve0)) * timeElapsed;
+                price1CumulativeLast += uint256(UQ112x112.encode(_reserve0).uqdiv(_reserve1)) * timeElapsed;
+            }
+        }
+        
+        
         reserve0 = uint112(balance0);
         reserve1 = uint112(balance1);
-        
+        blockTimestampLast = uint32(block.timestamp);
+
         emit Sync(reserve0, reserve1);
     }
 
--- test/Pair.t.sol
+++ test/Pair.t.sol
@@ -3,21 +3,31 @@ pragma solidity ^0.8.9;
 
 import "forge-std/Test.sol";
 import "../src/Pair.sol";
+import "./mocks/ERC20Mintable.sol";
+
 
 contract PairTest is Test {
     ERC20Mintable token0;
     ERC20Mintable token1;
-    ZuniswapV2Pair pair;
+    Pair pair;
+    TestUser testUser;
 
     function setUp() public {
+        testUser = new TestUser();
         token0 = new ERC20Mintable("A", "TKNA");
         token1 = new ERC20Mintable("B", "TKNB");
 
         pair = new Pair(address(token0), address(token1));
 
-        token0.mint(10 ether);
-        token1.mint(10 ether);
+        token0.mint(10 ether, address(testUser));
+        token1.mint(10 ether, address(testUser));
 
+    }
+
+    function assertReserves(uint112 expectedReserve0, uint112 expectedReserve1) internal {
+        (uint112 reserve0, uint112 reserve1, ) = pair.getReserves();
+        assertEq(reserve0, expectedReserve0, "unexpected reserve0");
+        assertEq(reserve1, expectedReserve1, "unexpected reserve1");
     }
 
     function testMintInitialSetup() public {
@@ -124,4 +134,23 @@ contract PairTest is Test {
 
     }
 
+}
+
+contract TestUser {
+    function provideLiquidity(
+        address pairAddress_,
+        address token0Address_,
+        address token1Address_,
+        uint256 amount0_,
+        uint256 amount1_
+    ) public {
+        ERC20(token0Address_).transfer(pairAddress_, amount0_);
+        ERC20(token1Address_).transfer(pairAddress_, amount1_);
+
+        Pair(pairAddress_).mint();
+    }
+
+    function withdrawLiquidity(address pairAddress_) public {
+        Pair(pairAddress_).burn();
+    }
 }
